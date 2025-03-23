2955f1d6e6f6c02a39611b8bc294ee68f0630969
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2025-03-23 19:42:26 -0400

    Iniciando el proyecto - Canvas y drawBall

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const canvas = document.querySelector("canvas");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const ctx = canvas.getContext("2d");
+
+    // VARIABLES DE LOS ELEMENTOS
+
+    canvas.width = 800;
+    canvas.height = 500;
+
+    // Tamaño de la pelota
+    const ballRadius = 4;
+
+    // Posicion de la pelota
+    let x = canvas.width / 2;
+    let y = canvas.height - 30;
+
+    // Velocidad de la Pelota
+    let dx = 2;
+    let dy = -2;
+
+    // FUNCIONES DE DIBUJADO
+    const drawBall = () => {
+      ctx.beginPath();
+      ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+      ctx.fillStyle = "#fff";
+      ctx.fill();
+      ctx.closePath();
+    };
+
+    const drawPaddle = () => {};
+    const drawBlock = () => {};
+
+    const ballMovement = () => {
+      // Colision paredes laterales
+      if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        // Invertir direccion en X
+        dx = -dx;
+      }
+
+      // Colision pared superior
+      if (y + dy < ballRadius) {
+        dy = -dy;
+      }
+
+      if (y + dy > canvas.height - ballRadius) {
+        // document.location.reload();
+        dy = -dy;
+      }
+
+      x += dx;
+      y += dy;
+    };
+
+    const collisionDetection = () => {};
+
+    const cleanCanvas = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    };
+
+    const draw = () => {
+      cleanCanvas();
+
+      // Apartado de Dibujado de elementos
+      drawBall();
+      drawPaddle();
+      drawBlock();
+
+      // Apartado de Movimiento y colisiones
+      ballMovement();
+
+      // Funcion para llamar constantemente y redibujar el canvas
+      window.requestAnimationFrame(draw);
+    };
+
+    draw();
+  });
+
+  return <canvas></canvas>;
 }
 
-export default App
+export default App;
